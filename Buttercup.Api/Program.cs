bf337bf22c8cebac58f52c2c00b7fc5000ade371
Stephen Feest <steve84@gmail.com>
2022-12-30 15:44:52 +0000

    Only serve Banana Cake Pop in development
    By default `MapGraphQL` configures the Hot Chocolate middleware to serve
    the Banana Cake Pop GraphQL IDE on the GraphQL endpoint [1]. However I
    feel that this significantly increases the attack surface of the
    application for little or no benefit outside of development.
    
    [1] https://chillicream.com/docs/hotchocolate/v12/server/endpoints#mapgraphql

--- Buttercup.Api/Program.cs
+++ Buttercup.Api/Program.cs
@@ -4,12 +4,17 @@ using Buttercup.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var isDevelopment = builder.Environment.IsDevelopment();
+
 builder.Services
     .AddGraphQLServer()
     .AddQueryType<Query>();
 
 var app = builder.Build();
 
-app.MapGraphQL();
+app.MapGraphQL().WithOptions(new()
+{
+    Tool = { Enable = isDevelopment }
+});
 
 app.Run();
