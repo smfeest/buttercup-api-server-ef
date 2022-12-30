6e92c99396e8b104f77c911077fafead4f72894a
Stephen Feest <steve84@gmail.com>
2022-12-30 15:43:00 +0000

    Add GraphQL endpoint
    The `Ping` field is just a placeholder. It's needed because the GraphQL
    specification mandates that every schema has a root `query` object type,
    and every object type needs to define at least one field.

---
+++ Buttercup.Api/Query.cs
@@ -0,0 +1,12 @@
+namespace Buttercup.Api;
+
+/// <summary>
+/// Represents the root `Query` type.
+/// </summary>
+public sealed class Query
+{
+    /// <summary>
+    /// Gets the string 'pong'.
+    /// </summary>
+    public string Ping => "pong";
+}
