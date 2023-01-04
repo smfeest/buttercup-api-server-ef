06600d92fe0d756086288deddd367b6ffdc63970
Stephen Feest <steve84@gmail.com>
2023-01-04 22:41:29 +0000

    Remove ping field
    Now that we have a real `users` field we no longer need the `ping` field
    to serve as a placeholder.

--- Buttercup.Api/Query.cs
+++ Buttercup.Api/Query.cs
@@ -8,11 +8,6 @@ namespace Buttercup.Api;
 public sealed class Query
 {
     /// <summary>
-    /// Gets the string 'pong'.
-    /// </summary>
-    public string Ping => "pong";
-
-    /// <summary>
     /// Returns an <see cref="IQueryable"/> for querying all users.
     /// </summary>
     /// <param name="dbContext">The database context.</param>
