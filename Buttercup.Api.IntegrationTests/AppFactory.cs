adf4ba29c89d335ec19ef314f1284d10343923ad
Stephen Feest <steve84@gmail.com>
2023-01-07 16:47:31 +0000

    Run integration tests against dedicated a database
    This will allow us to safely recreate the database on each run without
    affecting the application database in development.
    
    The explicit implementation of `IAsyncLifetime.DisposeAsync()` is needed
    because `WebApplicationFactory<T>` implements `IAsyncDisposable`, which
    defines a conflicting `DisposeAsync` method with `ValueTask` as the
    return type.

--- Buttercup.Api.IntegrationTests/AppFactory.cs
+++ Buttercup.Api.IntegrationTests/AppFactory.cs
@@ -1,9 +1,32 @@
+using Buttercup.Api.DbModel;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
 
 namespace Buttercup.Api.IntegrationTests;
 
-public class AppFactory : WebApplicationFactory<Query>
+public class AppFactory : WebApplicationFactory<Query>, IAsyncLifetime
 {
+    Task IAsyncLifetime.InitializeAsync() => this.RecreateDatabase();
+
+    async Task IAsyncLifetime.DisposeAsync() => await this.DisposeAsync();
+
     protected override void ConfigureWebHost(IWebHostBuilder builder) =>
-        builder.UseSetting("HostBuilder:ReloadConfigOnChange", bool.FalseString);
+        builder
+            .UseSetting(
+                "ConnectionStrings:AppDb",
+                "Host=localhost;Username=buttercup_dev;Password=buttercup_dev;Database=buttercup_test")
+            .UseSetting("HostBuilder:ReloadConfigOnChange", bool.FalseString);
+
+    private async Task RecreateDatabase()
+    {
+        using var scope = this.Services.CreateScope();
+
+        var contextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<AppDbContext>>();
+
+        using var context = await contextFactory.CreateDbContextAsync();
+
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+    }
 }
